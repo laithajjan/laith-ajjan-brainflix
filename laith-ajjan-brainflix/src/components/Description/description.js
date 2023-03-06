99e31cda75a805e0a4edcf011d1952e076424d7a
laithajjan <laithajjan@gmail.com>
2023-03-05 17:32:36 -0700

    Finished styling and reformatted some code

--- laith-ajjan-brainflix/src/components/Description/description.js
+++ laith-ajjan-brainflix/src/components/Description/description.js
@@ -1,37 +1,43 @@
+// Import React and the styles for this component
 import React from "react";
 import "./description.scss";
+
+// Import the images for the views and likes logos
 import viewsLogo from '../../assets/images/views.svg'
 import likesLogo from '../../assets/images/likes.svg'
 
+// Define the Description component
 const Description = (props) => {
 
-      const { title, channel, timestamp, views, likes, description } =
-      props.chosenVideo;
-      let date = new Date(timestamp).toLocaleDateString();
-    
+    // Destructure the props and get the needed values
+    const { title, channel, timestamp, views, likes, description } =
+        props.chosenVideo;
+
+    // Convert the timestamp to a date string
+    let date = new Date(timestamp).toLocaleDateString();
+
+    // Return the component with the video description information
     return (
         <div className="description">
-           <h1 className="description__title">{title}</h1>
-                        <hr className="description__break-small"></hr>
-                        <div className="description__info">   
-                            <div className="description__info--left">
-                                <h3 className="description__info--channel">By {channel}</h3>
-                                <p className="description__info--date">{date}</p>
-                            </div>
-                            <div className="description__info--right">
-                                <p className="description__info--views"><img alt="Views logo" src={viewsLogo} className="description__info--logo"/>{views}</p>
-                                <p className="description__info--likes"><img alt="Likes logo" src={likesLogo} className="description__info--logo"/>{likes}</p>
-                            </div>
-                        </div>
-                        <hr></hr>
-                        <p className="description__story">{description}</p>
-                        
-                        <div>
-
-
-                        </div>
+            <h1 className="description__title">{title}</h1>
+            <hr className="description__break-small"></hr>
+            <div className="description__info">
+                <div className="description__info--left">
+                    <h3 className="description__info--channel">By {channel}</h3>
+                    <p className="description__info--date">{date}</p>
+                </div>
+                <div className="description__info--right">
+                    <p className="description__info--views"><img alt="Views logo" src={viewsLogo} className="description__info--logo" />{views}</p>
+                    <p className="description__info--likes"><img alt="Likes logo" src={likesLogo} className="description__info--logo" />{likes}</p>
+                </div>
+            </div>
+            <hr></hr>
+            <p className="description__story">{description}</p>
+            <div>
+            </div>
         </div>
     )
 }
 
-export default Description
+// Export the Description component for use in other parts of the application
+export default Description;
