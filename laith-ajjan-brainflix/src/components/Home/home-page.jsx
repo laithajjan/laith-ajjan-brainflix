34ebcc9a60df4383ad6df624f29df95ed098063a
laithajjan <laithajjan@gmail.com>
2023-05-03 19:14:18 -0600

    Made some changes to homepage component for readability and optimization

--- laith-ajjan-brainflix/src/components/Home/home-page.jsx
+++ laith-ajjan-brainflix/src/components/Home/home-page.jsx
@@ -1,70 +1,63 @@
-// Import necessary libraries and components
-import React, { Component } from "react";
-import axios from "axios";
-import Video from "../Video/video";
-import Description from "../Description/description";
-import Comments from "../Comments/comments";
-import VideoGallery from "../Video-gallery/video-gallery";
+import React, { Component } from 'react';
+import axios from 'axios';
+import Video from '../Video/video';
+import Description from '../Description/description';
+import Comments from '../Comments/comments';
+import VideoGallery from '../Video-gallery/video-gallery';
 
-// HomePage component
+const API_BASE_URL = 'http://localhost:8080';
+
 class HomePage extends Component {
   state = {
     videoSuggest: [],
     chosenVideo: {},
   };
 
-  // Fetch video details by ID
-  getVideoById = (id) => {
-    axios
-      .get(`http://localhost:8080/videos/${id}`)
-      .then((response) => {
-        this.setState({
-          chosenVideo: response.data,
-        });
-      })
-      .catch((err) => console.log(err));
+  getVideoById = async (id) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/videos/${id}`);
+      this.setState({ chosenVideo: response.data });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  // Fetch initial data when the component mounts
-  componentDidMount() {
-    document.title = "BrainFlix";
-    axios
-      .get("http://localhost:8080/videos")
-      .then((response) => {
-        this.setState({
-          videoSuggest: response.data,
-        });
-        const videoId = this.props.match.params.videoId || response.data[0].id;
-        this.getVideoById(videoId);
-      })
-      .catch((err) => console.log(err));
+  async componentDidMount() {
+    document.title = 'BrainFlix';
+    try {
+      const response = await axios.get(`${API_BASE_URL}/videos`);
+      this.setState({ videoSuggest: response.data });
+      const videoId = this.props.match.params.videoId || response.data[0].id;
+      this.getVideoById(videoId);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  // Update chosen video when the video ID changes
   componentDidUpdate(prevProps, prevState) {
-    const videoId =
-      this.props.match.params.videoId || this.state.videoSuggest[0].id;
+    const videoId = this.props.match.params.videoId || this.state.videoSuggest[0].id;
 
-    if (prevState.chosenVideo && prevState.chosenVideo.id !== videoId) {
+    if (prevState.chosenVideo?.id !== videoId) {
       this.getVideoById(videoId);
     }
   }
 
-  // Render the HomePage component
   render() {
+    const { chosenVideo, videoSuggest } = this.state;
+
     return (
       <>
-        <Video chosenVideo={this.state.chosenVideo} />
+        <Video chosenVideo={chosenVideo} />
         <section className="main">
           <section className="main__left">
-            <Description chosenVideo={this.state.chosenVideo} />
-            <Comments chosenVideo={this.state.chosenVideo} />
+            <Description chosenVideo={chosenVideo} />
+            <Comments chosenVideo={chosenVideo} />
           </section>
           <section className="main__right">
             <VideoGallery
-              VideoDetails={this.state.videoSuggest}
+              VideoDetails={videoSuggest}
               getVideoById={this.getVideoById}
-              chosenVideo={this.state.chosenVideo}
+              chosenVideo={chosenVideo}
             />
           </section>
         </section>
